25814517c0e0fdb94e121aa303340f70c0387972
acallender65 <amaracallender655@gmail.com>
2024-12-30 23:08:27 +0000

    301224

--- Overworld.js
+++ Overworld.js
@@ -1,1 +1,24 @@
-console.log('')
+class Overworld {
+    constructor(config) {
+      this.element = config.element;
+      this.canvas = this.element.querySelector(".game-canvas");
+      this.ctx = this.canvas.getContext("2d");
+    }
+   
+    init() {
+      const map = new Image();
+      map.onload = () => {
+        this.ctx.drawImage(map,0,0)
+      };
+      map.src = "backgrounds-4.png.png";
+   
+      const character = new Image();
+      character.onload = () => {
+        this.ctx.drawImage(character, 0, 0)
+      }
+      character.src = "Actual Character.png";
+   
+   
+    }
+   
+   }
