516575c623fe7273f8fa634ad6eea3d2d8eb32be
B <140404758+5294593@users.noreply.github.com>
2024-12-31 18:48:07 +0000

    try2 initialising map + character

---
+++ OverworldMap.js
@@ -0,0 +1,39 @@
+class OverworldMap {
+    constructor(config) {
+        this.gameObjects = config.gameObjects;
+
+        this.lowerImage = new Image();
+        this.lowerImage.src = config.lowerSrc;
+
+        this.upperImage = new Image();
+        this.upperImage.src = config.upperSrc;
+    }
+
+    drawLowerImage(ctx) {
+        ctx.drawImage(this.lowerImage, 0, 0)
+    }
+
+    drawUpperImage(ctx) {
+        ctx.drawImage(this.upperImage, 0, 0)
+    }
+
+
+}
+
+window.OverworldMaps = {
+    Library: {
+        lowerSrc: "/images/maps/backgrounds-4.png.png",
+        upperSrc: "/images/maps/transparent.png",
+        gameObjects: {
+            mc : new GameObject({
+                x: 50, 
+                y: 150
+            }),
+            npc : new GameObject({
+                x: 50, 
+                y: 200,
+                src: "/images/characters/filler.png"
+            })
+        }
+    }
+}
